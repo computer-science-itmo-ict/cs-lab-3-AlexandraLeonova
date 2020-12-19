12699acb4ec9007d30779b878f62bfa31e6db46e
AlexandraLeonova <alexleonova2013@gmail.com>
2020-12-19 14:29:08 +0000

    3 lab..

--- script.bash
+++ script.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 export LANG=en_US.UFT-8
 if [ "$1" = "" ]
-	then
-	current_dir=$PWD
-	parameter="."
-	else
-	current_dir="$1"
-	parameter=$1
+  then
+  current_dir=$PWD
+  parameter="."
+  else
+  current_dir="$1"
+  parameter=$1
 fi
 
 echo "$parameter"
@@ -15,37 +15,37 @@ files_count=0
 dirs_count=0
 
 search() {
-  local dir=$1
-	 local parent=$2
+   local dir=$1
+       local parent=$2
 
-	 local list=($dir/*)
+       local list=($dir/*)
 
-	 local len_list="${#list[@]}"
+       local len_list="${#list[@]}"
 
-	 for each in "${!list[@]}"
-	 do
-			local parent_add=$'\u2502\u00A0\u00A0\u0020'
-			local child=$'\u251c\u2500\u2500\u0020'
+       for each in "${!list[@]}"
+       do
+		local parent_add=$'\u2502\u00A0\u00A0\u0020'
+		local child=$'\u251c\u2500\u2500\u0020'
 
-			local name=${list[$each]}
-			name=${name##*/}
+		local name=${list[$each]}
+		name=${name##*/}
 			
-			if [ $each -eq $((len_list-1)) ]
-			then
-				child=$'\u2514\u2500\u2500\u0020'
-				parent_add=$'\u0020\u0020\u0020\u0020'
-			fi
+		if [ $each -eq $((len_list-1)) ]
+		then
+			child=$'\u2514\u2500\u2500\u0020'
+			parent_add=$'\u0020\u0020\u0020\u0020'
+		fi
 			
-			echo "$parent$child$name"
+		echo "$parent$child$name"
 			
-			if [ -d "$dir/$name" ]
-			then
-				dirs_count=$((dirs_count+1))
-				search "$dir/$name" "$parent$parent_add"
-			else
-				files_count=$((files_count+1))
-			fi
-		done
+		if [ -d "$dir/$name" ]
+		then
+			dirs_count=$((dirs_count+1))
+			search "$dir/$name" "$parent$parent_add"
+		else
+			files_count=$((files_count+1))
+		fi
+	done
 }
 search "${current_dir}" ""
 echo
@@ -55,12 +55,12 @@ d="directory"
 d2="directories"
 
 if [[ $dirs_count -eq 1 ]]
-then sasha=$d
-else sasha=$d2
+then j=$d
+else j=$d2
 fi
 if [[ $files_count -eq 1 ]]
-then sashas=$f
-else sashas=$f2
+then k=$f
+else k=$f2
 fi
-echo "$dirs_count $sasha, $files_count $sashas"
+echo "$dirs_count $j , $files_count $k"
 
