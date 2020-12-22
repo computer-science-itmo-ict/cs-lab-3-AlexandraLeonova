04e1890f9bf4140d59d6d811d698ffb4b5946990
AlexandraLeonova <alexleonova2013@gmail.com>
2020-12-22 18:03:39 +0000

    3 task!

--- script.bash
+++ script.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
-export LANG=en_US.UFT-8
-if [ "$1" = "" ]
+export LANG=en_US.UTF-8
+if ["$1" = ""]
   then
-  current_dir=$PWD
-  parameter="."
+  dir=$PWD
+  parameter="." 
   else
-  current_dir="$1"
+  dir="$1"
   parameter=$1
 fi
 
@@ -16,56 +16,59 @@ dirs_count=0
 
 
 
+
 search() {
-   local dir=$1
-       local parent=$2
+    local dirf=$1
+	local parent=$2
+	
+	local list=($dirf/*)
+	
+	local len_list="${#list[@]}"
+	
 
-       local list=($dir/*)
-
-       local len_list="${#list[@]}"
-
-       for each in "${!list[@]}"
-       do
-
-
+	
+	
+	for each in "${!list[@]}"
+	do
+		
 		local parent_add=$'\u2502\u00A0\u00A0\u0020'
 		local child=$'\u251c\u2500\u2500\u0020'
-
+		
 		local name=${list[$each]}
 		name=${name##*/}
-
+			
 		if [ $each -eq $((len_list-1)) ]
 		then
 			child=$'\u2514\u2500\u2500\u0020'
 			parent_add=$'\u0020\u0020\u0020\u0020'
 		fi
-
-
+		
 		echo "$parent$child$name"
-
-		if [ -d "$dir/$name" ]
-		then
+		
+		if [ -d "$dirf/$name" ]
+		then 
 			dirs_count=$((dirs_count+1))
-			search "$dir/$name" "$parent$parent_add"
+			search "$dirf/$name" "$parent$parent_add"
 		else
 			files_count=$((files_count+1))
 		fi
 	done
 }
-search "${current_dir}" ""
+search "${dir}" ""
 echo
 f="file"
-f2="files"
+ff="files"
 d="directory"
-d2="directories"
+dd="directories"
 
 if [[ $dirs_count -eq 1 ]]
-then j=$d
-else j=$d2
+then te=$d
+else te=$dd
 fi
 if [[ $files_count -eq 1 ]]
-then k=$f
-else k=$f2
+then xt=$f
+else xt=$ff
 fi
-echo "$dirs_count $j , $files_count $k"
+echo "$dirs_count $te, $files_count $xt"
+
 
